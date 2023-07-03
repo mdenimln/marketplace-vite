5ba1bf02833d2b1942c4390cd1e0ae47718a7059
Deni Maulana <denimlnm@gmail.com>
2023-07-03 15:56:01 +0700

    add dbproducts.json

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,45 +1,9 @@
 import { Fragment, useState } from "react";
 import CartProducts from "../components/Fragments/CartProducts";
 import Button from "../components/Elements/Button";
+import Products from "./dbproducts.json"
 
 
-const products = [
-    {
-        id: "1",
-        image: "/images/shoes1.jpg",
-        name: "Product satu",
-        price: 200000,
-        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio qui quisquam, voluptatibus ipsa dolorem eligendi aut eaque quibusdam maxime officiis sed distinctio totam ipsam possimus id accusantium iusto dignissimos architecto?`
-    },
-    {
-        id: "2",
-        image: "/images/shoes1.jpg",
-        name: "Product dua",
-        price: 300000,
-        description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.`
-    },
-    {
-        id: "3",
-        image: "/images/shoes1.jpg",
-        name: "Product tiga",
-        price: 400000,
-        description: `Optio qui quisquam, voluptatibus ipsa dolorem eligendi aut eaque quibusdam maxime officiis sed distinctio totam ipsam possimus id accusantium iusto dignissimos architecto?`
-    },
-    {
-        id: "4",
-        image: "/images/shoes1.jpg",
-        name: "Product empat",
-        price: 400000,
-        description: `Optio qui quisquam, voluptatibus ipsa dolorem eligendi aut eaque quibusdam maxime officiis sed distinctio totam ipsam possimus id accusantium iusto dignissimos architecto?`
-    },
-    {
-        id: "5",
-        image: "/images/shoes1.jpg",
-        name: "Product lima",
-        price: 400000,
-        description: `Optio qui quisquam, voluptatibus ipsa dolorem eligendi aut eaque quibusdam maxime officiis sed distinctio totam ipsam possimus id accusantium iusto dignissimos architecto?`
-    },
-]
 const email = localStorage.getItem("email");
 const ProductsPage = () => {
     const [cart, setCart] = useState([]);
@@ -68,7 +32,7 @@ const ProductsPage = () => {
             </nav> 
             <div className="flex justify-center p-5 mt-28">
                 <div className="flex w-4/6 flex-wrap gap-5 border">           
-                    {products.map((product) => (
+                    {Products.map((product) => (
                         <CartProducts key={product.id}>
                             <CartProducts.Header image={product.image} />
                             <CartProducts.Body name={product.name}>{product.description}</CartProducts.Body>
@@ -93,15 +57,15 @@ const ProductsPage = () => {
                         </thead>
                         <tbody>
                             {cart.map((items) => {
-                                const product = products.find((product) => product.id === items.id);
+                                const Product = Products.find((product) => product.id === items.id);
                                 return (
                                    <tr key={items.id}>
-                                        <td>{product.name }</td>
-                                        <td>{product.price.toLocaleString("id-ID", { 
+                                        <td>{Product.name }</td>
+                                        <td>{Product.price.toLocaleString("id-ID", { 
                                             style: "currency", 
                                             currency: "IDR" }) }</td>
                                         <td>{items.qty }</td>
-                                        <td>{(items.qty * product.price).toLocaleString("id-ID", { style: "currency", currency: "IDR" }) }</td>
+                                        <td>{(items.qty * Product.price).toLocaleString("id-ID", { style: "currency", currency: "IDR" }) }</td>
                                    </tr> 
                                 )
                             })}
