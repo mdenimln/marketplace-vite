5ce2a1d0d47fabccbffe96f5e215ad682644a24a
Deni Maulana <denimlnm@gmail.com>
2023-07-09 22:09:17 +0700

    custom hooks login

---
+++ src/hooks/useLogin.jsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react";
+import { getUsername } from "../services/auth.service";
+export const useLogin = () => {
+    const [username, setUsername] = useState("");
+    useEffect(() => {
+
+        const token = localStorage.getItem("token");
+        if (token) {
+          setUsername(getUsername(token));
+        } else{
+          window.location.href = "/login";
+        }
+      }, []);
+      return  username ;
+}
