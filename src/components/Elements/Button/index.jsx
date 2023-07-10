16df49555529294e257cdf859c71333ac312fd0c
Deni Maulana <denimlnm@gmail.com>
2023-07-10 23:37:01 +0700

    config

--- src/components/Elements/Button/index.jsx
+++ src/components/Elements/Button/index.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line react/prop-types
-const Button = ({children, classname, onClick= () => {} , type="button"}) => {
+const Button = ({children, classname, onClick = () => {} , type="button"}) => {
     return(
       <button 
         className={`h-10 px-6 font-semibold rounded-md ${classname} text-white`}
