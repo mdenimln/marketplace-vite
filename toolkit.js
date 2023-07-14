b46763dcfbbc6280c3eaf067315412a1a7cafda0
Deni Maulana <denimlnm@gmail.com>
2023-07-14 22:02:11 +0700

    try toolkit

--- toolkit.js
+++ toolkit.js
@@ -3,7 +3,8 @@ import toolkit from "@reduxjs/toolkit";
 const { configureStore, createAction, createReducer } = toolkit;
 
 const addToCard = createAction("ADD_TO_CART");
-
+const login = createAction("LOGIN");
+// reducer
 const cartReducer = createReducer(
     [], (builder) => {
         builder.addCase(addToCard, (state, action) => {
@@ -11,15 +12,29 @@ const cartReducer = createReducer(
         })
     } 
 )
-
+const loginReducer = createReducer(
+    {status: false},
+    (builder) => {
+        builder.addCase(login, (state, action) => {
+            state.status = action.payload.status;
+        })
+    }
+)
+// store
 const store = configureStore({
     reducer: {
+        login: loginReducer,
         cart: cartReducer
-    }
+    }, 
 })
-console.log("store saat ini: ",store.getState());
+
+console.log("store sebelumnya: ",store.getState());
+
+// subscribe
 store.subscribe(() => {
     console.log("store saat ini: ",store.getState());
 })
 
+// dispatch
 store.dispatch(addToCard({id: 1, qty: 2}));
+store.dispatch(login({status: true}));
